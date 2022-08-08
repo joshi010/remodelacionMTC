9a2ec7add2537eb4920256f0f41f6549d3f6d886
j_venegas <josuesinvr1@gmail.com>
2022-08-08 16:12:24 -0500

    add js animation beta

---
+++ sources/js.js
@@ -0,0 +1,12 @@
+let flex1 = document.querySelector('.flex1');
+ 
+
+
+
+
+let nev = document.querySelector("nav");
+
+window.onload = () => {
+    nev.classList.toggle("show");
+    flex1.classList.toggle('up-to-bottom');
+}; 
