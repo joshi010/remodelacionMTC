6176a492463976d04ac055c3185d4497d6319349
j_venegas <josuesinvr1@gmail.com>
2022-08-09 17:38:32 -0500

    fix stetics of about page

---
+++ sources/dropDown.js
@@ -0,0 +1,20 @@
+let historyDrop = document.querySelector('#history');
+historyDrop.onclick = () => {
+    let ph = document.getElementById('name-history');
+    let p1 = document.getElementById('p1');
+    ph.classList.toggle('display-block');
+    p1.classList.toggle('rotate');
+    console.log('Niggers')
+}
+
+
+let more = document.querySelector('#more-tog');
+more.onclick = () => {
+    let m = document.getElementById('more');
+    let p2 = document.getElementById('p2');
+    m.classList.toggle('display-block');
+    p2.classList.toggle('rotate');
+    console.log('Niggers')
+}
+
+
--- sources/js.js
+++ sources/js.js
@@ -1,11 +1,5 @@
 let flex1 = document.querySelector('.flex1');
- 
-
-
-
-
 let nev = document.querySelector("nav");
-
 window.onload = () => {
     nev.classList.toggle("show");
     flex1.classList.toggle('up-to-bottom');
